aa5de0b550aa4d2ebe0d0a14b289778c70486e45
Cindy Jiang <xinming.jiang@mail.utoronto.ca>
2025-09-09 16:26:48 -0400

    change 2

--- src/lab1/Multiples.java
+++ src/lab1/Multiples.java
@@ -2,14 +2,20 @@ package lab1;
 
 public class Multiples {
     public static void main(String[] args) {
+
+        System.out.println(Multiples(1000,3,5));}
+
+    public static int Multiples(int a,int b,int c) {
         int count = 0;
-        for (int number = 1; number <= 1000; number++) {
-            boolean three = number % 3 == 0;
-            boolean five = number % 5 == 0;
-            if (three) {count++;}
-            if (five) {count++;}
+        for (int number = 1; number < a; number++) {
+            boolean three = number % b == 0;
+            boolean five = number % c == 0;
+            if (three && five)  {count++;}
+            else if (three) {count++;}
+            else if (five) {count++;}
+            else {}
             }
-
-    System.out.println(count);}
-
+        return count;
     }
+
+}
